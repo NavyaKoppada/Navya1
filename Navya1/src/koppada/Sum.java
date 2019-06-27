261ab23c9e683a41aeda8a1c0d45d7eb5895c059
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 20:16:53 +0530

    commit ok

---
+++ Navya1/src/koppada/Sum.java
@@ -0,0 +1,10 @@
+package koppada;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("the sum of " +a+ " and "+b+ " is " + (a+b));
+	}
+
+}
